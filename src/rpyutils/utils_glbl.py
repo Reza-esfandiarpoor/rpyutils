0bb073f13745296238caa2e4bdc4a7b85a0edcb3
Reza Esfandiarpoor <re.esfandiarpoor@gmail.com>
2023-09-06 20:13:25 -0400

    create global util functions

---
+++ src/rpyutils/utils_glbl.py
@@ -0,0 +1,52 @@
+"""Utils to be imported globally with star
+
+Generic utility functions
+"""
+
+import json
+import pickle as pkl
+from pathlib import Path
+
+
+def load_json(path, **kwargs):
+    with open(path, "r") as f:
+        obj = json.load(f, **kwargs)
+    return obj
+
+
+def write_json(obj, path, **kwargs):
+    path = Path(path)
+    path.parent.mkdir(exist_ok=True, parents=True)
+    with path.open("w") as f:
+        json.dump(obj, f, **kwargs)
+
+
+def read_json_lines(path, **kwargs):
+    with open(path, "r") as f:
+        lines = f.readlines()
+    obj_list = [
+        json.loads(line.strip(), **kwargs) for line in lines if line.strip() != ""
+    ]
+    return obj_list
+
+
+def write_json_lines(obj_list, path, **kwargs):
+    path = Path(path)
+    path.parent.mkdir(exist_ok=True, parents=True)
+    line_list = [json.dumps(item, **kwargs) for item in obj_list]
+    file_content = "\n".join(line_list)
+    with open(path, "w") as f:
+        f.write(file_content)
+
+
+def load_pickle(path, **kwargs):
+    with open(path, "rb") as f:
+        obj = pkl.load(f, **kwargs)
+    return obj
+
+
+def write_pickle(obj, path, **kwargs):
+    path = Path(path)
+    path.parent.mkdir(exist_ok=True, parents=True)
+    with open(path, "wb") as f:
+        pkl.dump(obj, f, **kwargs)
